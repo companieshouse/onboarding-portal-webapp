1bc1aadd5ecbdd8d759609eb22af4bd81245466a
gevans-ch <gevans@companieshouse.gov.uk>
2021-03-25 17:38:29 +0000

    Adding mock API feature flag to service

--- services/contentService.js
+++ services/contentService.js
@@ -1,16 +1,30 @@
 const axios = require("axios");
+const fs = require("fs");
 
 exports.getHomePages = async function () {
-  const url = process.env.CMS_API + "/home-pages";
-  return await axios.get(url)
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-      return response.data;
-    }).catch(function (error) {
-    // handle error
-    console.log(error);
-    throw error;
-  })
-
+    console.log(process.env.MOCK_API_RESPONSES)
+    if (process.env.MOCK_API_RESPONSES !== true) {
+        console.log("api_path")
+        const url = process.env.CMS_API + "/home-pages";
+        return await axios.get(url)
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+                return response.data;
+            }).catch(function (error) {
+                // handle error
+                console.log(error);
+                throw error;
+            })
+    } else {
+        try {
+            const rawResponse = fs.readFileSync("mockApiResponses/homepage.json", 'utf-8');
+            const response = JSON.parse(rawResponse);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
 }
