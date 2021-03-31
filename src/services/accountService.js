03f25932ad5d3b81d5e04a506aad67186531bebf
ICoxCH <ICox@companieshouse.gov.uk>
2021-03-31 14:02:28 +0100

    Remove uneeded log

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { setAuthTokenCookie } = require('./cookieService');
 
-exports.register = async function(res, username, email, password) {
+exports.register = async function (res, username, email, password) {
   const url = process.env.CMS_API + '/auth/local/register';
 
   // Request API.
@@ -10,11 +10,10 @@ exports.register = async function(res, username, email, password) {
     .post(url, {
       username: username,
       email: email,
-      password: password,
+      password: password
     })
     .then(response => {
       // Handle success.
-      console.log('Well done!');
       console.log('User profile', response.data.user);
       console.log('User token', response.data.jwt);
       setAuthTokenCookie(res, response.data.jwt);
@@ -24,19 +23,18 @@ exports.register = async function(res, username, email, password) {
       console.log('An error occurred:', error.response);
       throw error;
     });
-}
+};
 
-exports.login = async function(res, identifier, password) {
+exports.login = async function (res, identifier, password) {
   const url = process.env.CMS_API + '/auth/local';
   // Request API.
   await axios
     .post(url, {
       identifier: identifier,
-      password: password,
+      password: password
     })
     .then(response => {
       // Handle success.
-      console.log('Well done!');
       console.log('User profile', response.data.user);
       console.log('User token', response.data.jwt);
       setAuthTokenCookie(res, response.data.jwt);
@@ -46,4 +44,4 @@ exports.login = async function(res, identifier, password) {
       console.log('An error occurred:', error.response);
       throw error;
     });
-}
+};
