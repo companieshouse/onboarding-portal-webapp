a029b1c0539e5a737c906fbd7d637ab90bc204bf
ICoxCH <ICox@companieshouse.gov.uk>
2021-06-24 16:52:27 +0100

    Added healthcheck endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { loginPost, registerPost } from '../controllers/loginController';
 import { loginGet } from '../controllers/loginController';
 import { Router } from "express";
 import { page } from '../controllers/pageController';
+import { healthcheck } from '../controllers/healthCheckController';
 
 export const router: Router = express.Router();
 
@@ -14,3 +15,5 @@ router.post('/register', registerPost);
 router.get('/', homePage);
 
 router.get('/page/:page_id', page);
+
+router.get('/healthcheck', healthcheck);
